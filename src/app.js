e117afd64c54951b433d2da01458dfc0399c12bb
Gabriel Guedes <g4brielguedes@gmail.com>
2020-03-29 05:30:55 -0300

    Add: error handler middleware

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const routes = require('./routes');
+const { errorHandler } = require('./middlewares');
 
 const app = express();
 
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(errorHandler);
 
 app.use('/', routes);
 
---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,28 @@
+const config = require('../config');
+const logger = require('../log');
+
+const errorHandler = (error, req, res, next) => {
+  const { body, params, headers, route } = req;
+  const { level, stack, message } = error;
+
+  const data = {
+    request: {
+      body,
+      params,
+      headers,
+      route,
+    },
+    level,
+    stack,
+    message,
+  };
+
+  logger.error(data);
+  
+  if (config.env === 'prod')
+    res.status(400).json({ error: message })
+  else
+    res.status(400).json({ error })
+};
+
+module.exports = errorHandler;
